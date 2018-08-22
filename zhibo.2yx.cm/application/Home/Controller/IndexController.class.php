7bd2a8f66faff08b40048cc78c9d69a0856be97f
PQLMANAGER <207176243@qq.com>
2018-08-22 18:39:15 +0800

    测试直播源

--- zhibo.2yx.cm/application/Home/Controller/IndexController.class.php
+++ zhibo.2yx.cm/application/Home/Controller/IndexController.class.php
@@ -35,7 +35,8 @@ class IndexController extends HomebaseController {
 			
 			$indexLive[$k]['fans_nums']=M("users_attention")->where("touid={$v['uid']}")->count();
 		}
-		var_dump($indexLive);exit;
+
+		var_dump(C('DB_NAME'));exit;
 
 		$sort=array_column($indexLive,"fans_nums");
 		array_multisort($sort, SORT_DESC, $indexLive);
